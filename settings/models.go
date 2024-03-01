2ba399bc1e95c59a5afa9472caf5eb44919647c0
王佳函 <mr_jia_han@qq.com>
2024-03-01 22:25:58 +0800

    新增-项目启动加载配置

--- settings/models.go
+++ settings/models.go
@@ -2,11 +2,14 @@ package settings
 
 import (
 	"encoding/base64"
+	"fmt"
+	"github.com/NeverStopDreamingWang/goi"
 	"github.com/NeverStopDreamingWang/goi/crypto"
 	"github.com/NeverStopDreamingWang/goi/db"
 	"github.com/NeverStopDreamingWang/goi/migrate"
 	"github.com/NeverStopDreamingWang/goi/model"
-	"time"
+	"github.com/NeverStopDreamingWang/huayun/huayun"
+	"strconv"
 )
 
 func init() {
@@ -18,15 +21,14 @@ func init() {
 		},
 	}
 	migrate.SQLite3Migrate(SQLite3Migrations)
+
+	settings := &SettingsModel{}
+	settings.ReadSettings("PanelName", "Version")
+
 }
 
 // 配置表
 type SettingsModel struct {
-	Name            *string    `field_name:"name" field_type:"TEXT NOT NULL UNIQUE" json:"name"`
-	Value           *string    `field_name:"value" field_type:"TEXT NOT NULL" json:"value"`
-	Type            *string    `field_name:"type" field_type:"TEXT NOT NULL" json:"type"`
-	Desc            *string    `field_name:"desc" field_type:"TEXT" json:"desc"`
-	Update_Datetime *time.Time `field_name:"update_datetime" field_type:"DATETIME" json:"update_datetime"`
 	Name            *string `field_name:"name" field_type:"TEXT NOT NULL UNIQUE" json:"name"`
 	Value           *string `field_name:"value" field_type:"TEXT NOT NULL" json:"value"`
 	Type            *string `field_name:"type" field_type:"TEXT NOT NULL" json:"type"`
@@ -47,6 +49,81 @@ func (settingsModel SettingsModel) ModelSet() *model.SQLite3Settings {
 		MySettings: model.MySettings{},
 	}
 	return modelSettings
+}
+
+// 从数据库中加载配置
+func (settingsModel SettingsModel) ReadSettings(names ...string) {
+	// 连接数据库
+	sqlite3DB, err := db.SQLite3Connect("default")
+	if err != nil {
+		panic(fmt.Sprintf("连接 SQLite3 [default] 数据库 错误: %v", err))
+	}
+
+	settingsList := make([]*SettingsModel, 0)
+	sqlite3DB.SetModel(SettingsModel{})
+	if len(names) != 0 {
+		// err = sqlite3DB.Where("name in (?)", "'"+strings.Join(names, "','")+"'").Select(&settingsList)
+		settingsList = make([]*SettingsModel, len(names))
+		for i, name := range names {
+			data := &SettingsModel{}
+			err = sqlite3DB.Where("name=?", name).First(data)
+			settingsList[i] = data
+		}
+	} else {
+		err = sqlite3DB.Select(&settingsList)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	// 从数据库中加载配置
+	for _, item := range settingsList {
+		switch *item.Name {
+		case "NetWork":
+			huayun.Server.Settings.NET_WORK = *item.Value
+		case "BindAddress":
+			huayun.Server.Settings.BIND_ADDRESS = *item.Value
+		case "Port":
+			Port, _ := strconv.Atoi(*item.Value)
+			huayun.Server.Settings.PORT = uint16(Port)
+		case "Domain":
+			huayun.Server.Settings.Domain = *item.Value
+		case "SSL":
+			huayun.Server.Settings.SSL = goi.MetaSSL{
+				STATUS:    *item.Value == "true", // SSL 开关
+				TYPE:      "",
+				CERT_PATH: "",
+				KEY_PATH:  "",
+			}
+		case "SSLType":
+			huayun.Server.Settings.SSL.TYPE = *item.Value
+		case "CertPath":
+			huayun.Server.Settings.SSL.CERT_PATH = *item.Value
+		case "KeyPath":
+			huayun.Server.Settings.SSL.KEY_PATH = *item.Value
+		case "TimeZone":
+			huayun.Server.Settings.TIME_ZONE = *item.Value
+		case "EvictPolicy":
+			EvictPolicy, _ := strconv.Atoi(*item.Value)
+			huayun.Server.Cache.EVICT_POLICY = goi.EvictPolicy(EvictPolicy)
+		case "ExpirationPolicy":
+			ExpirationPolicy, _ := strconv.Atoi(*item.Value)
+			huayun.Server.Cache.EXPIRATION_POLICY = goi.ExpirationPolicy(ExpirationPolicy)
+		case "MaxSize":
+			MaxSize, _ := strconv.Atoi(*item.Value)
+			huayun.Server.Cache.MAX_SIZE = int64(MaxSize)
+		case "Debug":
+			huayun.Server.Log.DEBUG = *item.Value == "true"
+		case "SecretKey":
+			huayun.Server.Settings.SECRET_KEY = *item.Value
+		case "PrivateKey":
+			huayun.Server.Settings.PRIVATE_KEY = *item.Value
+		case "PublicKey":
+			huayun.Server.Settings.PUBLIC_KEY = *item.Value
+		default:
+			huayun.Server.Settings.Set(*item.Name, *item.Value)
+		}
+	}
 }
 
 func InItSettings() error {
@@ -75,27 +152,32 @@ func InItSettings() error {
 	publicKey := string(publicKeyBytes)
 
 	configData := [][]string{
-		// 面板设置
+		// 面板信息
 		{"PanelName", "华运面板", "string", "面板名称"},
 		{"Language", "zh", "string", "面板语言"},
 		{"Theme", "auto", "string", "面板主题"},
 		{"Version", "v0.1.0", "string", "面板版本"},
 		// 面板服务
+		{"NetWork", "tcp", "string", "网络协议"},
 		{"BindAddress", "0.0.0.0", "string", "绑定地址"},
-		{"BindDomain", "", "string", "绑定域名"},
 		{"Port", "8080", "int", "面板端口"},
-		{"Ipv6", "disable", "string", "Ipv6状态"},
-		{"DefaultNetwork", "all", "string", "默认网络"},
-		{"SSL", "disable", "string", "SSL状态"},
+		{"Domain", "", "string", "绑定域名"},
+		{"SSL", "false", "bool", "SSL状态"},
 		{"SSLType", "self", "string", "SSL类型"},
+		{"CertPath", "", "string", "SSL证书"},
+		{"KeyPath", "", "string", "SSL密钥"},
 		{"TimeZone", "Asia/Shanghai", "string", "面板时区"},
+		{"EvictPolicy", "2", "int", "缓存淘汰策略"},
+		{"ExpirationPolicy", "1", "int", "过期策略"},
+		{"MaxSize", "52428800", "int", "缓存大小"},
+		{"Debug", "true", "bool", "开发模式"},
 		// 其它设置
 		{"SecretKey", secretKey, "string", "AES密钥"},
 		{"PrivateKey", privateKey, "string", "RSA私钥"},
 		{"PublicKey", publicKey, "string", "RSA公钥"},
 		{"SystemIP", "", "string", "系统IP"},
 		{"SessionTimeout", "86400", "int", "Session过期时间"},
-		{"FileRecycleBin", "enable", "string", "文件回收站"},
+		{"FileRecycleBin", "true", "bool", "文件回收站"},
 	}
 
 	for _, item := range configData {
